c46e139ca60a040cc4addf0f5899369aa9f69cee
admit-me <immrpuneeth@gmail.com>
2026-09-10 23:59:12 +0530

    feat: add recruiter routes

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
 from . import views
-urlpatterns=[path('',views.job_list,name='job_list'),path('jobs/<int:pk>/',views.job_detail,name='job_detail'),path('jobs/<int:pk>/apply/',views.apply,name='apply'),path('applications/',views.my_applications,name='my_applications')]
+
+urlpatterns = [
+    path('', views.job_list, name='job_list'),
+    path('jobs/<int:pk>/', views.job_detail, name='job_detail'),
+    path('jobs/<int:pk>/apply/', views.apply, name='apply'),
+    path('applications/', views.my_applications, name='my_applications'),
+    path('recruiter/jobs/', views.recruiter_jobs, name='recruiter_jobs'),
+    path('recruiter/jobs/new/', views.create_job, name='create_job'),
+    path('recruiter/jobs/<int:pk>/applications/', views.manage_applications, name='manage_applications'),
+]
